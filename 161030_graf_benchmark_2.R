f51d10020671a96223d272cc5d34dd998db56d55
atajti <atajti@gmail.com>
2016-11-15 09:41:09 +0100

    hasonló userek listáin alapuló gráfok kuka, viszont a sima élerősséges gráf jónak tűnik

--- 161030_graf_benchmark_2.R
+++ 161030_graf_benchmark_2.R
@@ -158,3 +158,239 @@ for(lim in limits){
 }
 
 
+#--------------------------------------------------------------------#
+### Hasonló userek #############################
+#--------------------------------------------------------------------#
+
+train_test <- tr_tst(cetli_train,
+                     formula="owner.objectId + cetliId.objectId ~ product ")
+train_test2 <- copy(train_test)
+user_based_benchmark <- function(train_test, quant=0.75){
+ 
+  user_similarity_table <- melt.data.table(data=train_test$train,
+                                           id.vars="owner.objectId",
+                                           measure.vars=setdiff(names(train_test$train),
+                                                                c("owner.objectId",
+                                                                  "cetliId.objectId")),
+                                           variable.factor=FALSE)[!value==0] 
+
+  jaccard_index <- function(set1, set2){
+    return(length(intersect(set1, set2))/length(union(set1, set2)))
+  }
+
+  user_similarity_results <- as.data.table(t(combn(unique(train_test$train$owner.objectId),
+                                                        2)))[, sim:=NA_real_]
+
+  all_users <- unique(user_similarity_table$owner.objectId)
+
+  for(r in seq(nrow(user_similarity_results))){
+    set(user_similarity_results,
+        i=r,
+        j=3,
+        value=jaccard_index(user_similarity_table[
+                              owner.objectId==user_similarity_results[r, V1],
+                              variable],
+                            user_similarity_table[
+                              owner.objectId==user_similarity_results[r, V2],
+                              variable]))
+  }
+  
+  # plot(density(user_similarity_results$sim[user_similarity_results$sim<.2 & user_similarity_results$sim>.015]))
+  # 75. percentilistől
+  similarity_limit <- quantile(user_similarity_results$sim, quant)
+  user_pairs <- user_similarity_results[sim > similarity_limit,
+                                        .(V1, V2)]
+
+result_list <- NULL
+for(u in all_users){ 
+
+  users_to_graph <- c(u,
+                      user_pairs[V1==u, V2],
+                      user_pairs[V2==u, V1])
+
+  user_data <- train_test$train[owner.objectId %in% users_to_graph,]
+
+
+
+  long_form <- melt.data.table(data=copy(user_data)[, owner.objectId := NULL],
+                               id.vars="cetliId.objectId",
+                               variable.name="product",
+                               value.name="weight",
+                               variable.factor=FALSE)[
+    weight > 0, ]
+  edgelist <- merge(long_form, long_form,
+                    by="cetliId.objectId",
+                    allow.cartesian=TRUE,
+                    all=FALSE)[
+    , cetliId.objectId := NULL][
+    product.x != product.y, ][
+    as.character(product.x) > as.character(product.y), ][,
+    weight := weight.x][, `:=`(weight.x=NULL,
+                               weight.y=NULL)][, weight := weight/max(weight)]
+
+  # gráf
+  limit <- -1
+  product_graph <- graph_from_data_frame(edgelist, directed=FALSE)
+  # product_graph <- delete.edges(product_graph,
+  #                               which(E(product_graph)$weight > limit))
+  prcl_list <- lapply(which(user_data$owner.objectId==u),
+                      function(r){
+                        res <- prcl2(product_graph,
+                              unlist(user_data[r,
+                                             V(product_graph)$name,
+                                             with=FALSE]))
+                        return(res)
+                        })
+  user_cetli_prcl <- cbind(user_data[owner.objectId==u,
+        .(owner.objectId, cetliId.objectId)],
+    rbindlist(lapply(prcl_list,
+           function(x){
+            as.data.table(t(x))
+           }),
+      use.names=TRUE,
+      fill=TRUE))
+  result_list <- c(result_list, list(user_cetli_prcl))
+  }
+
+  res_for_all_cetli <- rbindlist(result_list,
+                                 use.names=TRUE,
+                                 fill=TRUE)
+
+  for(i in seq(ncol(res_for_all_cetli))){
+    set(res_for_all_cetli,
+        i=which(is.na(res_for_all_cetli[[i]])),
+        j=as.integer(i),
+        value=0)
+  }
+
+  mego <- merge(res_for_all_cetli,
+                train_test$test[, .(owner.objectId, cetliId.objectId, objectId)],
+                all.x=FALSE,
+                all.y=TRUE,
+                by=c("owner.objectId", "cetliId.objectId"))[, `:=`(owner.objectId=NULL,
+                                                                   cetliId.objectId=NULL)]
+  setcolorder(mego,
+              c("objectId",
+                sort(names(mego)[!names(mego)=="objectId"])))
+  return(mego)
+
+}
+
+mego <- user_based_benchmark(train_test, .75)
+cetli_eval(copy(train_test$test), mego)
+
+mego2 <- copy(mego)
+for(i in 2:(ncol(mego2))){
+  set(mego2, j=as.integer(i), value=mego2[[i]]^10)
+}
+
+cetli_eval(copy(train_test$test), mego2)
+
+
+mego <- user_based_benchmark(train_test, .5)
+cetli_eval(copy(train_test$test), mego)
+
+mego2 <- copy(mego)
+for(i in 2:(ncol(mego2))){
+  set(mego2, j=as.integer(i), value=mego2[[i]]^10)
+}
+
+cetli_eval(copy(train_test$test), mego2)
+
+
+mego <- user_based_benchmark(train_test, .25)
+cetli_eval(copy(train_test$test), mego)
+
+mego2 <- copy(mego)
+for(i in 2:(ncol(mego2))){
+  set(mego2, j=as.integer(i), value=mego2[[i]]^10)
+}
+
+cetli_eval(copy(train_test$test), mego2)
+
+
+quants <- c(.5, .6, .7, .75, .8, .85, .90, .95, .975, .99)
+quants <- quants[length(quants):1]
+num_of_experiments <- 10
+all_results <- matrix(NA_real_,
+                      ncol=length(quants),
+                      nrow=num_of_experiments)
+for(quant in quants){
+  print(paste0(quant, ": ", Sys.time()))
+
+  for(experiment in seq(num_of_experiments)){
+    print(paste0("experiment ", experiment, ": ", Sys.time()))
+    traintest <- tr_tst(cetli_train,
+                        formula="owner.objectId + cetliId.objectId ~ product ")
+    all_results[experiment, as.character(quant)] <- cetli_eval(traintest$test,
+                                                     user_based_benchmark(
+                                                      traintest,
+                                                      quant)) 
+  }
+}
+
+
+
+#--------------------------------------------------------------------#
+### Minden terméket figyelembe véve, de használva a súlyokat #########
+#--------------------------------------------------------------------#
+
+basic_prcl_bmrk <- function(train_test){
+
+  tr_wide <- train_test$train
+  tst_wide <- train_test$test
+
+  # éllista
+  long_form <- melt.data.table(data=tr_wide,
+                               id.vars="cetliId.objectId",
+                               variable.name="product",
+                               variable.factor=FALSE,
+                               value.name="weight")[
+    weight>0,]#[, value := NULL] 
+  edgelist <- merge(long_form, long_form,
+                    by="cetliId.objectId",
+                    allow.cartesian=TRUE,
+                    all=FALSE)[
+    , cetliId.objectId := NULL][
+    product.x != product.y, ][
+    as.character(product.x) > as.character(product.y), ]
+  weighted_edgelist <- edgelist[, .(weight=.N), by=.(product.x, product.y)]
+  weighted_edgelist[, weight := (weight/max(weight))*100]
+
+  # gráf
+  product_graph <- graph_from_data_frame(weighted_edgelist, directed=FALSE)
+
+  # minden listánál kiszámolom a prcl-t
+  prcl_list <- lapply(seq(nrow(tr_wide)),
+        function(r){
+          res <- prcl2(product_graph,
+                unlist(tr_wide[r,
+                               V(product_graph)$name,
+                               with=FALSE]))
+          if(!r%%100){
+            cat(paste0("\n", r, " : ", Sys.time()))
+          }
+          return(res)
+        })
+
+  # data-table-lé teszem, majd kiszedem belőle a teszt adatokat
+  res_wide <- rbindlist(lapply(prcl_list, as.list))[,
+    cetliId.objectId := tr_wide$cetliId.objectId]
+
+  res_wide <- res_wide[cetliId.objectId %in% tst_wide$cetliId.objectId,]
+
+  res_obj <- tst_wide[, .(cetliId.objectId, objectId)][
+                      res_wide,  on="cetliId.objectId"]
+
+  return(res_obj)
+}
+
+model_res <- basic_prcl_bmrk(train_test)
+
+cetli_eval(train_test$test, model_res)
+mego2 <- copy(res_obj)
+for(i in 3:(ncol(mego2))){
+  set(mego2, j=as.integer(i), value=mego2[[i]]^10)
+}
+
+cetli_eval(copy(train_test$test), mego2)
